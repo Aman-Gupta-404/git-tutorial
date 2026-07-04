65012e26d4e19c86c98cc1e797d7c4b88fa9d7c9
Aman-Gupta-404 <ag9139563@gmail.com>
2026-07-04 21:04:24 +0530

    implemented the second feature

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // this is my first comment
 
 console.log("this is my first line of code!");
+
+console.log("this is my second feture code");
+
+/*
+-- git init --> initializes the git in project
+
+-- get status
+
+
+*/
